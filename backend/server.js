13b243d2b3dfa1fb38f73ab6d782136cc498d205
khu5shi <khushi4608@gmail.com>
2026-05-06 22:37:58 +0530

    fixed the backend issue

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/reports", reportRoutes);
 
 //Start Server 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
